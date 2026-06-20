e8ac714da5a0f8e204fea0a32e0bd5708a53ec12
rishiraj65 <160401306+rishiraj65@users.noreply.github.com>
2026-06-20 17:26:01 +0530

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -14,6 +14,9 @@ function getTransporter() {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      connectionTimeout: 10000, // 10 seconds
+      greetingTimeout: 10000,   // 10 seconds
+      socketTimeout: 10000,     // 10 seconds
     });
   }
   return transporter;
